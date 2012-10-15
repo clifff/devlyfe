fc89845a9151ca7e07fc0f885014021d5bd9cba1
Clif Reeder <clif@sbnation.com>
2012-10-14 21:42:05 -0400

    have capistrano run main.rb after each deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,11 @@
+require "bundler/capistrano"
+
 set :application, "devlyfe"
 set :repository,  "git://github.com/clifff/devlyfe.git"
 
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "linode"                          # Your HTTP server, Apache/etc
+role :web, "linode"
 set :user , "devlyfe"
 set :deploy_to,  "/home/devlyfe"
 set :use_sudo, false
@@ -14,8 +15,6 @@ set :deploy_via, :remote_cache
 # if you want to clean up old releases on each deploy uncomment this:
 after "deploy:restart", "deploy:cleanup"
 
-
-task :bundle_install do
-  run "cd #{release_path} && bundle install --deployment"
+before "deploy:create_symlink" do 
+  run "cd #{release_path} && bundle exec ruby main.rb"
 end
-after "deploy:update_code", "bundle_install"
